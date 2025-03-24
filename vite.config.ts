023598f36aac1eed68b12ed475d8e2e1e50457be
Zhi-Hahn See <zsee@usc.edu>
2025-03-24 13:48:44 -0700

    Fix link

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,9 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+
+  base: "/zhi-hahn-see-portfolio/",
+
   server: {
     host: "::",
     port: 8080,
